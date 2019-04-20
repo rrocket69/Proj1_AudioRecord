e2eb7f7d2be1f4cd6d17430c5b47af9ad4b19eaf
rrocket_69 <veniamin1601@mail.ru>
2019-04-21 01:37:37 +0300

    added tests

--- src/test/java/Testing.java
+++ src/test/java/Testing.java
@@ -1,8 +1,39 @@
+import controller.Factory;
+import entities.Playable;
+import entities.Styles;
+import model.Disc;
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class Testing {
-    @Test
-    public void testing(){
+    Disc disc;
+    @Before
+    public void setUp(){
+        disc = new Disc();
+        disc.addSong(Factory.createOne("Song","Hey Jude", Styles.Rock, "The Beatles", 200000));
+        disc.addSong(Factory.createOne("Opera","Ghost of opera", Styles.Classical, "Webber", 500000));
+        disc.addSong(Factory.createOne("Track","Yo", Styles.HipHop, "Noname", 199999));
+        disc.addSong(Factory.createOne("Symphony","6th", Styles.Classical, "Mozart", 400001));
+        disc.addSong(Factory.createOne("Song","What a wonderful world", Styles.Jazz, "Lui Armstrong", 200001));
 
     }
+    @Test
+    public void testDurationOfAll(){
+        assertEquals(1500001,disc.calculateDuration());
+    }
+    @Test
+    public void testOrder(){
+        Playable ghost = disc.getPlayableImpl(1);
+
+        assertEquals(ghost.play(),disc.getPlayableImpl(1).play());
+        disc.bubbleSortByStyle();
+        assertEquals(ghost.play(),disc.getPlayableImpl(0).play());
+    }
+    @Test
+    public void testDurationDiapason(){
+        assertEquals("[ HipHop Yo ]",disc.findByDiapasonDuration(100000,200000));
+    }
+
 }
